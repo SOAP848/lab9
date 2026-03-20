09646f191d6d33787345f6cc5c11e5fe21dc267f
Soap <artyom.krivoxin@mail.ru>
2026-03-20 17:21:35 +0300

    refactor: extract calc package for testability

--- mid4/go/main.go
+++ mid4/go/main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"math"
 	"os"
+
+	"mid4/calc"
 )
-
-type Request struct {
-	Task string  `json:"task"`
-	A    float64 `json:"a"`
-	B    float64 `json:"b"`
-}
-
-type Response struct {
-	Result float64 `json:"result,omitempty"`
-	Error  string  `json:"error,omitempty"`
-}
 
 func main() {
 	input, err := io.ReadAll(os.Stdin)
@@ -27,15 +16,15 @@ func main() {
 		return
 	}
 
-	var req Request
+	var req calc.Request
 	if err := json.Unmarshal(input, &req); err != nil {
 		writeErrorAndExit(fmt.Errorf("invalid json: %w", err), 1)
 		return
 	}
 
-	res, err := handle(req)
+	res, err := calc.Compute(req)
 	if err != nil {
-		_ = json.NewEncoder(os.Stdout).Encode(Response{Error: err.Error()})
+		_ = json.NewEncoder(os.Stdout).Encode(calc.Response{Error: err.Error()})
 		os.Exit(1)
 		return
 	}
@@ -43,21 +32,8 @@ func main() {
 	_ = json.NewEncoder(os.Stdout).Encode(res)
 }
 
-func handle(req Request) (Response, error) {
-	switch req.Task {
-	case "add":
-		return Response{Result: req.A + req.B}, nil
-	case "mul":
-		return Response{Result: req.A * req.B}, nil
-	case "pow":
-		return Response{Result: math.Pow(req.A, req.B)}, nil
-	default:
-		return Response{}, errors.New("unknown task: " + req.Task)
-	}
-}
-
 func writeErrorAndExit(err error, code int) {
-	_ = json.NewEncoder(os.Stdout).Encode(Response{Error: err.Error()})
+	_ = json.NewEncoder(os.Stdout).Encode(calc.Response{Error: err.Error()})
 	os.Exit(code)
 }
 
